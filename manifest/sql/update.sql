1d20bf983246744e6b941ccdf67bc182a06cc8b1
maqinglin <singledemon@sina.com>
2023-09-08 11:03:08 +0800

    添加sql修改记录

--- manifest/sql/update.sql
+++ manifest/sql/update.sql
@@ -2,7 +2,7 @@
 -- 2023-09-08 修改字典类型表
 -- ----------------------------
 ALTER TABLE `sagoo_iot`.`sys_dict_data`
-    ADD COLUMN `dict_class` int NULL DEFAULT 0 COMMENT '字典分类：取值从字典类型' AFTER `remark`;
+    ADD COLUMN `dict_class` varchar(10) NOT NULL DEFAULT 0 COMMENT '字典分类：取值从字典类型' AFTER `remark`;
 ALTER TABLE `sagoo_iot`.`sys_dict_type`
     MODIFY COLUMN `is_deleted` int NOT NULL DEFAULT 0 COMMENT '是否删除 0未删除 1已删除' AFTER `status`;
 ALTER TABLE `sagoo_iot`.`sys_dict_type`
